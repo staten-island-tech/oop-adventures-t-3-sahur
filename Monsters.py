afab70c7a0b4d3603dfe395c99b7466682998265
BrandonWang269 <brandonw269@nycstudents.net>
2026-05-07 10:17:18 -0400

    smdsa

---
+++ Monsters.py
@@ -0,0 +1,20 @@
+class Enemy:
+    def __init__(self, name, hp, atk, mp, inventory, skills):
+        self.__name = name
+        self.__hp = hp
+        self.__atk = atk
+        self.__mp = mp
+        self.__inventory = inventory
+        self.__skills = skills
+    def show_enemy(self):
+        print(f"{self.__name}'s status: ")
+        print(f"Health: {self.__hp}")
+        print(f"Mana: {self.__mp}")
+        print(f"Attack: {self.__atk}")
+        print(f"Inventory: {self.__inventory}")
+        print(f"Skills: {self.__skills}")
+
+Goblin = Enemy("Goblin", 45, 3, 5, {"title": "Wooden Dagger", "atk": 2}, {"title": "Stab", "atk": 2, "mp": -1})
+Goblin_Archer = Enemy("Goblin Archer", 35, 4, 10, {"title": "Short Bow", "atk": 3}, {"title": "Aim", "atk": 5, "mp": -2})
+Goblin_Brute = Enemy("Goblin Brute", 75, 2, 5, {"title": "Leather Armor", "hp": 10}, {"title": "Taunt", "atk": 2})
+enemies_list = [Goblin, Goblin_Archer, Goblin_Brute]
