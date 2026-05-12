6c6c8e09312a9bb58d6319b21d95c1a9bfef7968
carsonp45 <carsonp3@nycstudents.net>
2026-05-12 10:17:09 -0400

    d

--- Heros.py
+++ Heros.py
@@ -1,4 +1,3 @@
-import random 
 class Hero:
     def __init__(self, name, exp, lvl, hp, mp, atk, inventory, skills):
         self.name = name
@@ -16,18 +15,12 @@ class Hero:
         self.mp += 5
         self.atk += 2
         print(f"{self.name} leveled up to {self.lvl}!")
-class Enemy:
-     def __init__(self, name, hp, atk, mp, inventory, skills):
-        self.name = name
-        self.hp = hp
-        self.atk = atk
-        self.mp = mp
-        self.inventory = inventory
-        self.skills = skills
-Goblin = Enemy("Goblin", 45, 3, 5, {"title": "Wooden Dagger", "atk": 2}, {"title": "Stab", "atk": 2})
-Goblin_Archer = Enemy("Goblin Archer", 35, 4, 10, {"title": "Short Bow", "atk": 3}, {"title": "Aim", "atk": 5})
-Goblin_Brute = Enemy("Goblin Brute", 75, 2, 5, {"title": "Leather Armor", "hp": 10}, {"title": "Taunt", "mp": -2})
-Gavin = Hero("Gavin", 0, 1, 100, 10, 5, {"title": "Wooden Sword", "atk": 2}, {"title": "Slash", "atk": 3})
-enemies = [Goblin, Goblin_Archer, Goblin_Brute]
-Fight = random.choice(enemies)
-print(f"Gavin encounters a {Fight.name}!")
+    def show_status(self):
+        print(f"{self.__name}'s status: ")
+        print(f"Level: {self.__lvl}")
+        print(f"Experience: {self.__exp}")
+        print(f"Health: {self.__hp}")
+        print(f"Mana: {self.__mp}")
+        print(f"Attack: {self.__atk}")
+        print(f"Inventory: {self.__inventory}")
+        print(f"Skills: {self.__skills}")
--- test.py
+++ test.py
@@ -1,2 +1,30 @@
-class Hero:
-    def __init__(self, name, atk, hp, mp):
+import random
+from Heros import Hero
+from Enemies import Enemy
+from Enemies import enemies_list
+Gavin = Hero("Gavin", 0, 1, 100, 10, 5, {"title": "Wooden Sword", "atk": 2}, {"title": "Slash", "atk": 3})
+
+def attack(self, target):
+        damage = self.__atk
+        print(f"{self.__name} attacks!")
+        target.take_damage(damage)
+
+current_enemy = random.choice(enemies_list)
+Option = input("Pick a choice. 1. Fight 2. Rest")
+if Option == "2":
+    Gavin.hp += 10
+    print(f"Gavin is now well rested. Hp is now {Gavin.hp}")
+elif Option == "1":
+    print(f"Gavin encounters a {current_enemy.name}!")
+
+    print("What action would you like to do:")
+    print("1. Attack")
+    print("2. Slash")
+    choice = input("Pick 1 or 2:")
+
+    if choice == "1":
+        Gavin.atk(current_enemy)
+    if choice == "2":
+        slash_dmg = Gavin.atk + 3
+else:
+        print("Invalid choice")
