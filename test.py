1f87a85949ac7ed43097fb462fe125fae854e17f
carsonp45 <carsonp3@nycstudents.net>
2026-05-13 10:16:59 -0400

    d

--- test.py
+++ test.py
@@ -3,28 +3,28 @@ from Heros import Hero
 from Enemies import Enemy
 from Enemies import enemies_list
 Gavin = Hero("Gavin", 0, 1, 100, 10, 5, {"title": "Wooden Sword", "atk": 2}, {"title": "Slash", "atk": 3})
-
-def attack(self, target):
-        damage = self.__atk
-        print(f"{self.__name} attacks!")
-        target.take_damage(damage)
-
-current_enemy = random.choice(enemies_list)
+Gavin.show_status
+Fight = random.choice(enemies_list)
 Option = input("Pick a choice. 1. Fight 2. Rest")
 if Option == "2":
     Gavin.hp += 10
     print(f"Gavin is now well rested. Hp is now {Gavin.hp}")
 elif Option == "1":
-    print(f"Gavin encounters a {current_enemy.name}!")
-
+    print(f"Gavin encounters a {Fight.name}!")
+    Fight.show_enemy()
     print("What action would you like to do:")
     print("1. Attack")
     print("2. Slash")
     choice = input("Pick 1 or 2:")
 
     if choice == "1":
-        Gavin.atk(current_enemy)
+        print(f"Gavin attacks {Fight.name}!")
+    Fight.hp -= Gavin.atk
+    print(f"{Fight.name}'s hp is now {Fight.hp}")
     if choice == "2":
-        slash_dmg = Gavin.atk + 3
+     print(f"Gavin slashes {Fight.name}!")
+    slash_dmg = Gavin.atk + 3
+    Fight.hp -= slash_dmg
+    print(f"{Fight.name}'s hp is now {Fight.hp}")
 else:
         print("Invalid choice")
