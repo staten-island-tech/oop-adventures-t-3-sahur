0f336e54405c2c2d3e90b5567273151613a74102
BrandonWang269 <brandonw269@nycstudents.net>
2026-05-14 10:14:23 -0400

    dsa

--- Monsters.py
+++ Monsters.py
@@ -1,4 +1,4 @@
-""" class Enemy:
+class Enemy:
     def __init__(self, name, hp, atk, mp, inventory, skills):
         self.name = name
         self.hp = hp
@@ -22,45 +22,7 @@
         print(f"{self.name} has died.")
 
 
-Goblin = Enemy("Goblin", 45, 3, 5, {"title": "Wooden Dagger", "atk": 2}, {"title": "Stab", "atk": 2, "mp": -1})
-Goblin_Archer = Enemy("Goblin Archer", 35, 4, 10, {"title": "Short Bow", "atk": 3}, {"title": "Aim", "atk": 5, "mp": -2})
-Goblin_Brute = Enemy("Goblin Brute", 75, 2, 5, {"title": "Leather Armor", "hp": 10}, {"title": "Taunt", "atk": 2})
+Goblin = Enemy("Goblin", 50, 3, 5, {"title": "Wooden Dagger", "atk": 2}, {"title": "Stab", "atk": 2, "mp": -1})
+Goblin_Archer = Enemy("Goblin Archer", 40, 4, 10, {"title": "Short Bow", "atk": 3}, {"title": "Aim", "atk": 5, "mp": -2})
+Goblin_Brute = Enemy("Goblin Brute", 80, 2, 5, {"title": "Leather Armor", "hp": 10}, {"title": "Taunt", "atk": 2})
 enemies_list = [Goblin, Goblin_Archer, Goblin_Brute]
- """
-
-import random
-class Enemy:
-    def __init__(self, name, hp, atk, mp, inventory, skills):
-        self.name = name
-        self.hp = hp
-        self.atk = atk
-        self.mp = mp
-        self.inventory = inventory
-        self.skills = skills
-    def show_enemy(self):
-        print(f"{self.name}'s status: ")
-        print(f"Health: {self.hp}")
-        print(f"Mana: {self.mp}")
-        print(f"Attack: {self.atk}")
-        print(f"Inventory: {self.inventory}")
-        print(f"Skills: {self.skills}")
-    def damage(self, amount):
-        self.hp -= amount
-        if self.hp < 0:
-            self.hp = 0
-        print(f"{self.name} too {amount} damage. Hp is now {self.hp}")
-    def alive(self):
-        return self.hp > 0
-
-
-
-
-
-
-
-
-Goblin = Enemy("Goblin", 45, 3, 5, {"title": "Wooden Dagger", "atk": 2}, {"title": "Stab", "atk": 2, "mp": -1})
-Goblin_Archer = Enemy("Goblin Archer", 35, 4, 10, {"title": "Short Bow", "atk": 3}, {"title": "Aim", "atk": 5, "mp": -2})
-Goblin_Brute = Enemy("Goblin Brute", 75, 2, 5, {"title": "Leather Armor", "hp": 10}, {"title": "Taunt", "atk": 2})
-enemies_list = [Goblin, Goblin_Archer, Goblin_Brute]
-
--- fight.py
+++ fight.py
@@ -1,58 +1,19 @@
-""" import random
-from Heros import Hero
-from Monsters import enemies_list
-from Monsters import Enemy
-
-Bob = Hero("Bob", 0, 100, 1, 100, 10, 5, {"title": "Iron Sword", "atk": 2}, {"title": "Slash", "atk": 3, "mp": -2})
-Fight = random.choice(enemies_list)
-Option = input("What would you like to do? Fight or rest: ")
-if Option == "rest":
-    Bob.hp += 10
-    print(f"Bob is well rested. Hp is now {Bob.hp}")
-elif Option == "fight":
-    print("Battle Start!")
-    Fight.show_enemy()
-    print("What action do you take:")
-    print("1: Attack")
-    print("2: Slash")
-    choice = input("Pick 1 or 2: ")
-
-    if choice == "1":
-        print(f"Bob attacks {Fight.name}")
-        Fight.hp -= Bob.atk
-    elif choice == "2":
-        print("Bob slashes")     
-    else:
-        print("Invalid choice")
- """
-
 import random
 from Heros import Hero
-from Monsters import Enemy
 from Monsters import enemies_list
-Gavin = Hero("Gavin", 0, 1, 100, 10, 5, {"title": "Wooden Sword", "atk": 2}, {"title": "Slash", "atk": 3})
-Gavin.show_status
+from Monsters import Enemy
+
+Gavin = Hero("Gavin", 0, 100, 1, 100, 10, 5, {"title": "Wooden Sword", "atk": 2}, {"title": "Slash", "atk": 3, "mp": -2})
 Fight = random.choice(enemies_list)
-Option = input("Pick a choice. 1. Fight 2. Rest")
+round = 0
+Option = input("Pick a choice: 1. Fight 2. Rest")
 if Option == "2":
     Gavin.hp += 10
-    print(f"Gavin is now well rested. Hp is now {Gavin.hp}")
+    print(f"Gavin is well rested. Hp is now {Gavin.hp}")
 elif Option == "1":
-   
-    print(f"Gavin encounters a {Fight.name}!")
-    Fight.show_enemy()
-    print("What action would you like to do:")
-    print("1. Attack")
-    print("2. Slash")
-    choice = input("Pick 1 or 2:")
-    if choice == "1":
-        print(f"Gavin attacks {Fight.name}!")
-    Fight.hp -= Gavin.atk
-    print(f"{Fight.name}'s hp is now {Fight.hp}")
-    if choice == "2":
-     print(f"Gavin slashes {Fight.name}!")
-    slash_dmg = Gavin.atk + 3
-    Fight.hp -= slash_dmg
-    print(f"{Fight.name}'s hp is now {Fight.hp}")
+    while Gavin.hp >= 0 or Fight.hp >= 0:
+        print(f"Gavin encounters a {Fight.name}")
+        Fight.show_enemy()
+        print(f"Round {round}")
 else:
-        print("Invalid choice")
+    print("Invalid choice")
