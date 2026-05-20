e1ff767650fb28f70abca12155c62231b4f6a517
BrandonWang269 <brandonw269@nycstudents.net>
2026-05-20 10:14:50 -0400

    dsa

--- fight.py
+++ fight.py
@@ -14,29 +14,37 @@ elif Option == "1":
     print(f"Gavin encounters a {Fight.name}")
     Fight.show_enemy()
     print(f"Round {round}")
-    print("What action do you like to do:")
-    print("1: Attack")
-    print("2: Slash")
     while Gavin.hp >= 0 or Fight.hp >= 0:
+        print("What action do you like to do:")
+        print("1: Attack")
+        print("2: Slash")
         choice = input("Pick 1 or 2: ")
         if choice == "1":
             print(f"Gavin attacks {Fight.name}")
             atk_dmg = Gavin.atk + 2
-            Fight.hp -= Gavin.atk
+            Fight.hp -= atk_dmg
             print(f"{Fight.name}'s hp is now {Fight.hp}")
             round += 1
             Gavin.mp += 1
-        elif choice == "2":
+        elif choice == "2": 
             print(f"Gavin slashes {Fight.name}")
+            Gavin.mp -= 3
             slash_dmg = Gavin.atk + 5
             Fight.hp -= slash_dmg
             print(f"{Fight.name}'s hp is now {Fight.hp}")
             round += 1
             Gavin.mp += 1
-        if Gavin.hp <= 0 or Fight.hp <= 0:
+        if Fight.hp <= 0:
             print("The battle is over")
             Gavin.exp += 15
             Gavin.show_status()
             break
-    else:
-        print("Invalid choice")
+        if Gavin.hp <= 0:
+            print(f"{Gavin.name} has fallen")
+            break
+        else:
+            print("Invalid choice")
+else:
+    print("Invalid choice")
+    Gavin.show_status()
+    Gavin.hp -= 10
