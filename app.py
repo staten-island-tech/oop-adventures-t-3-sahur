bfc42ef79996cafe0d7bf5b89ad10a874493332f
BrandonWang269 <brandonw269@nycstudents.net>
2026-05-28 10:15:02 -0400

    sSADS

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ from Heros import Hero
 
 Gavin = Hero("Gavin", 0, 100, 1, 100, 10, 5, {"title": "Wooden Sword", "atk": 2}, {"title": "Slash", "atk": 3, "mp": -2})
 
-while Gavin.hp > 0:
-    Option
+Option
